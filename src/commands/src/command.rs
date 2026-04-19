1538d19a677a6cf071f56d080b8cbc91e48e6d7f
Kapperchino <kamuran12580@gmail.com>
2026-04-19 18:58:03 -0400

    type

---
+++ src/commands/src/command.rs
@@ -0,0 +1,56 @@
+use nucleo::pattern::{CaseMatching, Normalization};
+use nucleo::{Config, Nucleo};
+use std::sync::Arc;
+use strum::VariantNames;
+use strum_macros::{EnumString, VariantNames};
+
+pub struct CommandContext {
+    nucleo: Nucleo<String>,
+}
+
+impl CommandContext {
+    pub fn new() -> CommandContext {
+        // there will be no new matcher states
+        let notify = Arc::new(|| {});
+        let nucleo = Nucleo::<String>::new(Config::DEFAULT, notify, Some(1), 1);
+        let injector = nucleo.injector();
+        Command::print_all().into_iter().for_each(|x| {
+            injector.push(x, |item, cols| {
+                cols[0] = item.as_str().into();
+            });
+        });
+        CommandContext { nucleo }
+    }
+
+    pub fn search(&mut self, string: &str) -> Vec<String> {
+        self.nucleo
+            .pattern
+            .reparse(0, string, CaseMatching::Smart, Normalization::Smart, false);
+
+        self.nucleo.tick(5);
+
+        let snapshot = self.nucleo.snapshot();
+        snapshot
+            .matched_items(..)
+            .into_iter()
+            .map(|x| x.data.clone())
+            .collect()
+    }
+}
+
+#[derive(Debug, PartialEq, EnumString, VariantNames)]
+#[strum(serialize_all = "lowercase")]
+pub enum Command {
+    PrintContext,
+    Logout,
+    Clear,
+}
+
+impl Command {
+    pub fn print_all() -> Vec<String> {
+        Command::VARIANTS
+            .into_iter()
+            .map(|x: &&str| x.to_string())
+            .collect()
+    }
+}
